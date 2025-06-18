6281abb9978975b6d847e6b8b679b7f2a028ae8a
seiobata <s.bautista18@yahoo.com>
2025-06-18 09:29:45 -0500

    generate model response in main

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    res = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    )
+
+    print(res.text)
+    print("Prompt tokens:", res.usage_metadata.prompt_token_count)
+    print("Response tokens:", res.usage_metadata.candidates_token_count)
+
+if __name__ == "__main__":
+    main()
