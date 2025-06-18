59f11cd71f72992f6f0bb56d1f61c44fc6c60739
seiobata <s.bautista18@yahoo.com>
2025-06-18 11:37:21 -0500

    refactor to track messages instead of single prompts

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
 
     if len(sys.argv) < 2:
         print("Error: No prompt provided.")
         sys.exit(1)
     prompt = " ".join(sys.argv[1:])
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
     res = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=prompt
+        contents=messages
     )
 
     print(res.text)
