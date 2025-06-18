b69de74fc6a174a461eb667e07bca38ae1688906
seiobata <s.bautista18@yahoo.com>
2025-06-18 12:28:43 -0500

    add verbose flag

--- main.py
+++ main.py
@@ -8,26 +8,37 @@ from google.genai import types
 def main():
     load_dotenv()
 
-    if len(sys.argv) < 2:
+    # create a list of arguments that don't start with --
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+
+    if len(args) < 1:
         print("Error: No prompt provided.")
         sys.exit(1)
-    prompt = " ".join(sys.argv[1:])
+
+    prompt = " ".join(args) # join arguments into a sentence
+    verbose = "--verbose" in sys.argv
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)])
     ]
 
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-    
+    if verbose:
+        print("User prompt:", prompt)
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
     res = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages
     )
 
+    if verbose:
+        print("Prompt tokens:", res.usage_metadata.prompt_token_count)
+        print("Response tokens:", res.usage_metadata.candidates_token_count)
     print(res.text)
-    print("Prompt tokens:", res.usage_metadata.prompt_token_count)
-    print("Response tokens:", res.usage_metadata.candidates_token_count)
 
 if __name__ == "__main__":
     main()
